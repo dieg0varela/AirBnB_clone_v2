009889a4570091afa0ff802ebc06f44590ad9258
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-08 20:31:14 +0000

    adding new exercises

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if (cls is None):
+            return FileStorage.__objects
+
+        ret_dic = {}
+        for key in FileStorage.__objects:
+            if (FileStorage.__objects[key].__class__ == cls):
+                ret_dic[key] = FileStorage.__objects[key]
+        return ret_dic
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +31,16 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes an obj from _objects"""
+        key = ""
+        if (obj is not None):
+            key = obj.__class__.__name__ + "." + obj.id
+        if (key == "") or (key not in FileStorage.__objects.keys()):
+            return
+
+        del FileStorage.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
