330a414ea681dcba8920afa7d9034f43fb114867
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-10 13:34:17 +0000

    asd

--- models/user.py
+++ models/user.py
@@ -11,9 +11,9 @@ from models import storage
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=False)
-    last_name = Column('last_name', String(128), nullable=False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
     #places = relationship("Place", backref="user",
                               #cascade="all, delete-orphan")
