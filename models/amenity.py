53e72d3e285f37c89eec7406a227fd10e0697e21
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-10 18:47:30 +0000

    asd

--- models/amenity.py
+++ models/amenity.py
@@ -9,4 +9,4 @@ from sqlalchemy.sql.sqltypes import String
 class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenities")
+    # place_amenities = relationship("Place", secundary="place_amenities")
