fd269129a4785b7ba85352fce345b9aa25830e52
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-10 13:26:13 +0000

    aaaaaaaaa

--- models/place.py
+++ models/place.py
@@ -6,13 +6,11 @@ from sqlalchemy.sql.sqltypes import Float, Integer, String
 from models.base_model import BaseModel, Base
 
 
-
-
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), primary_key=True, nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=False)
     number_rooms = Column(Integer, default=0, nullable=False)
