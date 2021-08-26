515796804a9867f1387642600b567a98d8d5a523
Diego Varela <2806@holbertonschool.com>
2021-08-26 15:02:46 +0000

    Fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,8 @@ printf %s "<html>
   </body>
 </html>
 " > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/releases/test/ /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _/a \        location /hbnb_static/ {\n      alias /data/web_static/current/;\n      autoindex on;\n }\n" /etc/nginx/sites-available/default
 service nginx restart
+exit 0
