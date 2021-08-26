fa12993dccceb219cd947b90db366f38fe5ed940
Diego Varela <2806@holbertonschool.com>
2021-08-26 14:51:20 +0000

    Upload setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Script that configure the deployment of hbnb
+apt update -y
+apt install nginx -y
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+printf %s "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School HBNB
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i "/server_name _/a \        location /hbnb_static/ {\n      alias /data/web_static/current/;\n      autoindex on;\n }\n" /etc/nginx/sites-available/default
+service nginx restart
