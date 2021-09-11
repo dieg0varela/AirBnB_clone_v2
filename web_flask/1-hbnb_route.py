7bfab8225670019737d53606f96eb69effc879bd
Diego Varela <2806@holbertonschool.com>
2021-09-11 21:44:41 +0000

    Add Comments task 0-1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-'''Flask task 0 Module'''
+'''Flask task 1 Module'''
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,6 +8,12 @@ app = Flask(__name__)
 def hello_world():
     '''Print Hello HBNB'''
     return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Print HBNB'''
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
