ebcabd8c55d910c595725e266ce19efcc752f4a7
Diego Varela <2806@holbertonschool.com>
2021-09-12 18:53:32 +0000

    Upload task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Flask task 1 Module'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''Print Hello HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Print HBNB'''
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    '''Print C with the variable text'''
+    res = text.replace("_", " ")
+    return 'C {}'.format(res)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
