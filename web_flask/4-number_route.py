059e423cefd866da42a3111f5029da1e09f56a62
Diego Varela <2806@holbertonschool.com>
2021-09-12 20:27:15 +0000

    Upload task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+'''Flask task 1 Module'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    '''Print Hello HBNB'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Print HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    '''Print C with the variable text'''
+    res = text.replace("_", " ")
+    return 'C {}'.format(res)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python(text="is_cool"):
+    '''Print Python with the variable text'''
+    res = text.replace("_", " ")
+    return 'Python {}'.format(res)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''Print N is a number'''
+    return '{:d} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
