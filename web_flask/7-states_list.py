394fb50b6ce9c9e6f8077d952d2ef812d5642f38
Diego Varela <2806@holbertonschool.com>
2021-09-13 00:49:09 +0000

    Fix pep8 task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     '''Print StatesList'''
-    return render_template('/7-states_list.html', states=storage.all(State).values())
+    return render_template('/7-states_list.html',
+                           states=storage.all(State).values())
 
 
 @app.teardown_appcontext
