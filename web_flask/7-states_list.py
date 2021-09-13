b0ace7bb4222d0f1d45400e12fb1a5bbf8c0ef43
Diego Varela <2806@holbertonschool.com>
2021-09-13 00:46:32 +0000

    Upload task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Flask task 1 Module'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    '''Print StatesList'''
+    return render_template('/7-states_list.html', states=storage.all(State).values())
+
+
+@app.teardown_appcontext
+def teardown(f):
+    '''Execute each time after an method'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
